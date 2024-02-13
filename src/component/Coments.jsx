571ae69c3fc4fd109fbe37818db52dd205d575b7
Sanalemba991 <154108599+Sanalemba991@users.noreply.github.com>
2024-02-13 10:20:02 +0530

    New Add

--- src/component/Coments.jsx
+++ src/component/Coments.jsx
@@ -22,7 +22,7 @@ function Comments(props) {
            <div className='flex'>
            <input onChange={(e)=>setComments(e.target.value)} type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="commets" required/>
             <button onClick={addComents} class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded ml-3">
-            Add
+            Add Button
             </button>
            </div>
            
